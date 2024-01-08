1b88f6c365ebf6163d9c4b23042122d39dd3e84c
Pratyaksh Prabhav Rao <pr2257@log-2.hpc.nyu.edu>
2024-01-08 15:43:58 -0500

    training attitude predictor with delta on HPC

--- scripts/sbatch/train.sh
+++ scripts/sbatch/train.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#                  R   B        model_type     unroll_length  
-sbatch job.sbatch  0   512           tcn             20                 
-sbatch job.sbatch  1   512           tcn             25
-sbatch job.sbatch  2   512           tcn             30
-sbatch job.sbatch  3   512           tcn             35
-sbatch job.sbatch  4   512           gru             20                
-sbatch job.sbatch  5   512           gru             25
-sbatch job.sbatch  6   512           gru             30
-sbatch job.sbatch  7   512           gru             35       
+#                  R   B        model_type     unroll_length  delta
+sbatch job.sbatch  0   512           gru             1         True  
+sbatch job.sbatch  1   512           gru             20        True
+sbatch job.sbatch  2   512           gru             20        True
+sbatch job.sbatch  3   512           gru             1         False
+sbatch job.sbatch  4   512           gru             20        False
+sbatch job.sbatch  5   512           gru             20        False

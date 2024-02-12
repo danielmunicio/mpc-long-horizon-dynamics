1a2564126616cae3a1786c6516179dfeb267deef
pratyaksh10 <praopratyaksh1@gmail.com>
2024-02-12 14:15:57 -0500

    Updated HPC training script

--- scripts/sbatch/train.sh
+++ scripts/sbatch/train.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-#                  R   B        model_type     unroll_length  delta
-sbatch job.sbatch  0   512           gru             1         True  
-sbatch job.sbatch  1   512           gru             20        True
-sbatch job.sbatch  2   512           gru             20        True
-sbatch job.sbatch  3   512           gru             1         False
-sbatch job.sbatch  4   512           gru             20        False
-sbatch job.sbatch  5   512           gru             20        False
+#                  R   B        model_type     unroll_length      encoder_sizes      decoder_sizes
+sbatch job.sbatch  0   2048            mlp             1            '512'            '256,128,128'
+sbatch job.sbatch  1   2048            mlp             5            '512'            '256,128,128'
+sbatch job.sbatch  2   2048            mlp             10           '512'            '256,128,128'
+sbatch job.sbatch  3   2048           lstm             1            '256'            '256,128,128'
+sbatch job.sbatch  4   2048           lstm             5            '256'            '256,128,128'
+sbatch job.sbatch  5   2048           lstm            10            '256'            '256,128,128'
+sbatch job.sbatch  6   2048           gru              1            '256'            '256,128,128'
+sbatch job.sbatch  7   2048           gru              5            '256'            '256,128,128'
+sbatch job.sbatch  8   2048           gru             10            '256'            '256,128,128'
+sbatch job.sbatch  9   2048           tcn              1            '256,128,128'    '256,128,128'
+sbatch job.sbatch  10  2048           tcn              5            '256,128,128'    '256,128,128'
+sbatch job.sbatch  11  2048           tcn             10            '256,128,128'    '256,128,128'
+sbatch job.sbatch  12  2048           transformer      1            '256'            '256,128,128'
+sbatch job.sbatch  13  2048           transformer      5            '1024'           '256,128,128'
+sbatch job.sbatch  14  2048           transformer     10            '1024'           '256,128,128'
